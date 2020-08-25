5dd07f51758e3d4068f3bc1fb1180c64aacf3e77
issam <issam.aboulfadl05@gmail.com>
2020-08-25 22:41:59 +0100

    add acount component sign up with the input

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -11,21 +11,67 @@ const SignUpPage = () => {
     )
 }
 
+const INITIA_STATE = {
+    username:'',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+}
+
 class SignUpForm extends Component {
     constructor(props){
         super(props);
+        this.state = {...INITIA_STATE};
     }
     onSubmit = event => {
 
     }
     onChange = event => {
-
+        this.setState({ [event.target.name]: event.target.value });
     }
     
     render(){
+        const {
+        username,
+        email,
+        passwordOne,
+        passwordTwo,
+        error,
+        } = this.state;
         return(
             <form onSubmit={this.onSubmit}>
+            <input
+             name="username"
+             value={username}
+             onChange={this.onChange}
+             type="text"
+             placeholder="Full Name"
+            />
+            <input
+             name="email"
+             value={email}
+             onChange={this.onChange}
+             type="text"
+             placeholder="Email Adresse"
+            />
+            <input
+             name="passwordOne"
+             value={passwordOne}
+             onChange={this.onChange}
+             type="password"
+             placeholder="Password"
+            />
+            <input
+             name="passwordTwo"
+             value={passwordTwo}
+             onChange={this.onChange}
+             type="password"
+             placeholder="Confirm Password"
+            />
+            <button type="submit">Sign Up</button>
 
+            {error && <p>{error.message}</p>}
             </form>
         );
     }
